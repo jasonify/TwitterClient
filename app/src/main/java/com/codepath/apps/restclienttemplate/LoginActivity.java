70029edab61df1a33cbaaed2db1378d255fbc65c
jason <bjason@gmail.com>
2017-09-26 04:57:53 -0700

    Twitter credentials + adding recycler view

--- app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
@@ -3,6 +3,7 @@ package com.codepath.apps.restclienttemplate;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 import com.codepath.oauth.OAuthLoginActionBarActivity;
 
@@ -26,6 +27,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	// i.e Display application "homepage"
 	@Override
 	public void onLoginSuccess() {
+		Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
 		// Intent i = new Intent(this, PhotosActivity.class);
 		// startActivity(i);
 	}
