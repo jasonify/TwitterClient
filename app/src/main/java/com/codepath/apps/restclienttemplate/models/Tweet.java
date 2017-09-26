292a0b411252e37740bd5bb581ed645e85e690d5
jason <bjason@gmail.com>
2017-09-26 10:35:05 -0700

    Tweet class

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,27 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by jason on 9/26/17.
+ */
+
+public class Tweet {
+
+    // our attributes
+    public String body;
+    public long uid; // databse ID for tweet
+    // public User user;
+    public String createdAt;
+
+    // deserailize the JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException{
+        Tweet tweet = new Tweet();
+
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        return tweet;
+    }
+}
