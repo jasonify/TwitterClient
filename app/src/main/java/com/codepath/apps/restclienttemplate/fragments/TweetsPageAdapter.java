b2fb97db8293aab42c5a185c3886f0d49f636d05
jason <bjason@gmail.com>
2017-10-04 09:26:18 -0700

    Moving over to tab bar

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsPageAdapter.java
@@ -0,0 +1,41 @@
+package com.codepath.apps.restclienttemplate.fragments;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by jason on 10/4/17.
+ */
+
+public class TweetsPageAdapter extends FragmentPagerAdapter {
+
+    private String tabTitles[] = new String[] {"Home", "Mentions"};
+    private Context context;
+
+    public TweetsPageAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new HomeTimelineFragment();
+        } else if (position == 1) {
+            return new MentionsTimelineFragment();
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
